eab8276107cb4eecad5d181182e7a81f4756878f
chi <chi0726nami@gmail.com>
2018-12-10 09:38:54 +0800

    gucci

--- index.js
+++ index.js
@@ -8,11 +8,21 @@
 
 const Vec3 = require('tera-vec3');
 
-module.exports = function EyyHanSoloLevelPragmatismFuckEthicsBitchSuckABigOneAyy(mod) {
-	const chestIds = [81341, 81342];
-	const data = {
-		9031: { redirect: new Vec3(72416, 133868, -503) },
-		9032: { redirect: new Vec3(28194, 179672, -1675) },
+module.exports = function FastSolo(mod) {
+	const chestIds = [81341, 81342],
+	data = {
+		7005: {
+			spawn: new Vec3(-481, 6301, 1956),
+			redirect: new Vec3(-341, 8665, 2180)
+		},
+		9031: {
+			spawn: new Vec3(68482, 125779, 776),
+			redirect: new Vec3(72416, 133868, -503)
+		},
+		9032: {
+			spawn: new Vec3(16921, 177244, -1664),
+			redirect: new Vec3(28194, 179672, -1675)
+		},
 		9713: {
 			spawn: new Vec3(54997, 116171, 4517),
 			redirect: new Vec3(52230, 117225, 4352)
@@ -20,23 +30,15 @@ module.exports = function EyyHanSoloLevelPragmatismFuckEthicsBitchSuckABigOneAyy
 		9777: {
 			spawn: new Vec3(-121630, -41458, 2398),
 			redirect: new Vec3(-112672, -35058, 410)
-		},
-		7005: {
-			spawn: new Vec3(-481, 6301, 1956),
-			redirect: new Vec3(-341, 8665, 2180)
 		}
 	};
 
-	let enabled = true;
-	let reset = false;
-
-	let inAkasha,
-	inBaracos;
+	let enabled = true,
+	reset = false;
 
 	mod.game.me.on('change_zone', (zone) => {
-		if (!enabled) return;
-		inAkasha = (zone == 9031);
-		inBaracos = (zone == 9032);
+		if (!enabled)
+			return;
 		if (zone == 9714 && reset) {
 			mod.send('C_RESET_ALL_DUNGEON', 1, {});
 			reset = false;
@@ -45,14 +47,16 @@ module.exports = function EyyHanSoloLevelPragmatismFuckEthicsBitchSuckABigOneAyy
 	})
 
 	mod.hook('S_SPAWN_ME', 3, event => {
-		if (!enabled || !data[mod.game.me.zone]) return;
-		if (event.loc.equals(data[mod.game.me.zone].spawn) || inAkasha || inBaracos)
+		if (!enabled || !data[mod.game.me.zone])
+			return;
+		if (event.loc.equals(data[mod.game.me.zone].spawn))
 			event.loc = data[mod.game.me.zone].redirect;
 		return true;
 	})
 
 	mod.hook('S_SPAWN_NPC', 9, event => {
-		if (!enabled) return;
+		if (!enabled)
+			return;
 		if (event.huntingZoneId == 713 && chestIds.includes(event.templateId)) {
 			reset = true;
 			mod.command.message('Ghillieglade will be reset the next time you enter Velik Sanctuary.');
@@ -60,7 +64,8 @@ module.exports = function EyyHanSoloLevelPragmatismFuckEthicsBitchSuckABigOneAyy
 	})
 
 	mod.hook('C_RESET_ALL_DUNGEON', 1, event => {
-		if (!enabled) return;
+		if (!enabled)
+			return;
 		if (mod.game.me.zone == 9713) {
 			reset = false;
 			mod.command.message('Ghillieglade was reset manually.');
