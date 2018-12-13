17ba429460b9d0fe60e9187f74d328f3bf817ee0
chi <chi0726nami@gmail.com>
2018-12-13 15:38:40 +0800

    camera

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-/**
- ** 9031 Akasha
- ** 9032 Baracos
- ** 9713 Ghillie
- ** 9777 Channelworks
- ** 7005 Velika
- **/
-
 const Vec3 = require('tera-vec3');
 
 module.exports = function FastSolo(mod) {
 	const chestIds = [81341, 81342],
 	data = {
-		7005: {
+		7005: { // Velika
 			spawn: new Vec3(-481, 6301, 1956),
-			redirect: new Vec3(-341, 8665, 2180)
+			redirect: new Vec3(-341, 8665, 2180),
+			w: -0.96
 		},
-		9031: {
+		9031: { // Akasha
 			spawn: new Vec3(68482, 125779, 776),
 			redirect: new Vec3(72416, 133868, -503)
 		},
-		9032: {
+		9032: { // Baracos
 			spawn: new Vec3(16921, 177244, -1664),
 			redirect: new Vec3(28194, 179672, -1675)
 		},
-		9713: {
+		9713: { // Ghillieglade
 			spawn: new Vec3(54997, 116171, 4517),
-			redirect: new Vec3(52230, 117225, 4352)
+			redirect: new Vec3(52230, 117225, 4352),
+			w: 1.6
 		},
-		9777: {
+		9777: { // Channelworks
 			spawn: new Vec3(-121630, -41458, 2398),
 			redirect: new Vec3(-112672, -35058, 410)
 		}
@@ -37,8 +31,7 @@ module.exports = function FastSolo(mod) {
 	reset = false;
 
 	mod.game.me.on('change_zone', (zone) => {
-		if (!enabled)
-			return;
+		if (!enabled) return;
 		if (zone == 9714 && reset) {
 			mod.send('C_RESET_ALL_DUNGEON', 1, {});
 			reset = false;
@@ -47,16 +40,17 @@ module.exports = function FastSolo(mod) {
 	})
 
 	mod.hook('S_SPAWN_ME', 3, event => {
-		if (!enabled || !data[mod.game.me.zone])
-			return;
-		if (event.loc.equals(data[mod.game.me.zone].spawn))
+		if (!enabled || !data[mod.game.me.zone]) return;
+		if (event.loc.equals(data[mod.game.me.zone].spawn)) {
 			event.loc = data[mod.game.me.zone].redirect;
+			if (data[mod.game.me.zone].w)
+				event.w = data[mod.game.me.zone].w;
+		}
 		return true;
 	})
 
 	mod.hook('S_SPAWN_NPC', 10, event => {
-		if (!enabled)
-			return;
+		if (!enabled) return;
 		if (event.huntingZoneId == 713 && chestIds.includes(event.templateId)) {
 			reset = true;
 			mod.command.message('Ghillieglade will be reset the next time you enter Velik Sanctuary.');
@@ -64,8 +58,7 @@ module.exports = function FastSolo(mod) {
 	})
 
 	mod.hook('C_RESET_ALL_DUNGEON', 1, event => {
-		if (!enabled)
-			return;
+		if (!enabled) return;
 		if (mod.game.me.zone == 9713) {
 			reset = false;
 			mod.command.message('Ghillieglade was reset manually.');
